c7cc23827c1fa5998ca1f34080191da5673fc771
Kelly Keng <kelly.keng1@gmail.com>
2022-05-18 14:20:24 -0400

    working app!

--- RevConnectApp/src/app/pages/postfeed/postfeed.component.ts
+++ RevConnectApp/src/app/pages/postfeed/postfeed.component.ts
@@ -17,25 +17,29 @@ export class Post{
   templateUrl: './postfeed.component.html',
   styleUrls: ['./postfeed.component.css']
 })
-export class PostfeedComponent implements OnInit {
 
+
+export class PostfeedComponent implements OnInit {
 public posts: Array<any> =[];  
   constructor( private httpClient: HttpClient ) 
-  {
-   }
+  {}
+   
    getPosts()
-  {
-    this.httpClient.get<any>('https://localhost:7161/api/Posts').subscribe(
-      response => {
-      console.log(response);
-      for (let index = 0; index < response.length; index++) {
-        this.posts.push(new Post(response[index].postID, response[index].body, response[index].date, response[index].image, response[index].postComment,response[index].postLikes));
-      }
-    });
-  }
+    {
+      console.log("i ran this function");
+      this.httpClient.get<any>('https://localhost:7140/api/Posts').subscribe(
+        response => {
+        console.log(response);
+        for (let index = 0; index < response.length; index++)
+        {
+          this.posts.push(new Post(response[index].postID, response[index].body, response[index].date, response[index].image, response[index].postComment,response[index].postLikes));
+        }
+      });
+    }
 
-  ngOnInit(): void {
-    this.getPosts;
-  }
+    ngOnInit(): void {
+      this.getPosts();
+      console.log("i ran ngOnInit");
+    }
 
 }
